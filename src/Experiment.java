2feb9e54c8872c8b696295cdcd58a0f6f33178a7
Sorin Gore <soringoredev@gmail.com>
2025-11-29 12:06:50 +0200

    Adskgfvnwr

--- src/Experiment.java
+++ src/Experiment.java
@@ -1,2 +1,10 @@
 public class Experiment {
+    public static void main(String[] args) {
+
+        System.out.println("Valentin Munteanu");
+        System.out.println("Valentin Munteanu");
+        System.out.println("Valentin Munteanu");
+        System.out.println("Valentin Munteanu");
+
+    }
 }
